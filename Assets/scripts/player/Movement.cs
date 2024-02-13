8a82a17ba5991802c1cf4d040accf6afcddecb25
Tobkubos <tobkub7@gmail.com>
2024-02-13 16:25:12 +0100

    mikrofala + skakanie

--- Assets/scripts/player/Movement.cs
+++ Assets/scripts/player/Movement.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-	private float moveSpeed = 7;    //PREDKOSC
-	private float maxSpeed = 7.5f;  //MAKS bo przez Force predkosc sie zwieksza
-	private float drag = 5;			//ograniczenie slizgania sie przez force
+	private float moveSpeed = 7f;		 //PREDKOSC
+	private float maxSpeed = 7.5f;		 //MAKS bo przez Force predkosc sie zwieksza
+	private float drag = 5f;			 //ograniczenie slizgania sie przez force
+	private float jumpforce = 5f;		 //moc skoku
 	public Transform orientation;
 	Vector3 moveDIrection;
 
@@ -23,6 +24,11 @@ public class Movement : MonoBehaviour
 	private void Update()
     {
 		KeyInput();
+
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Jump();
+		}
     }
 	
 	private void KeyInput()
@@ -47,5 +53,21 @@ public class Movement : MonoBehaviour
 		{
 			rb.AddForce(moveDIrection.normalized * moveSpeed * 10f, ForceMode.Force);
 		}
+	}
+
+	private void Jump()
+	{
+		// SprawdŸ, czy postaæ znajduje siê na ziemi, aby unikn¹æ podwójnego skoku
+		if (IsGrounded())
+		{
+			rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+		}
+	}
+
+	private bool IsGrounded()
+	{
+		// SprawdŸ, czy postaæ jest na ziemi (na podstawie niewielkiego przesuniêcia od dolnej krawêdzi collidera)
+		float distanceToGround = GetComponent<Collider>().bounds.extents.y;
+		return Physics.Raycast(transform.position, -Vector3.up, distanceToGround + 0.1f);
 	}
 }
